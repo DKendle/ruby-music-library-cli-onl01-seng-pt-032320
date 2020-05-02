8138e6e1f67e7a5759e9399b173457f846620eaa
Dina Kendle <dina.kendle@gmail.com>
2020-05-02 20:15:24 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -83,6 +83,7 @@ class MusicLibraryController
           song = self.list_songs[input+2]
           puts "Playing #{song.name} by #{song.artist.name}"
         end
+      end
   # def play_song
   #   puts "Which song number would you like to play?"
   #   input = gets.chomp.to_i
