1e8d6aa198a5c8af0c7863258388150927cd237d
Dina Kendle <dina.kendle@gmail.com>
2020-05-02 17:30:18 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -39,7 +39,8 @@ class MusicLibraryController
   end
 
   def list_genres
-    
+    Genre.all.sort {|a, b| a.name <=> b.name}.uniq.each.with_index(1) do |genre, i|
+      puts "#{i}. #{genre.name}"
   end
 
 
