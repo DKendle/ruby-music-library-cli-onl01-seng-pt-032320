25469c4c5e642c3f49bd63faf78ca5744ddade65
Dina Kendle <dina.kendle@gmail.com>
2020-05-02 21:09:55 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -74,8 +74,8 @@ class MusicLibraryController
 
       if (input > 0) && (input <= Song.all.length)
         # Song.all.sort{|indexed_song_obj| indexed_song_obj[[input-1]] }
-        song = Song.all[input-1]
-# binding.pry
+        song = Song.all{|a, b| a.name <=> b.name}[input-1]
+binding.pry 
         puts "Playing #{song.name} by #{song.artist.name}" if song
 
       end
