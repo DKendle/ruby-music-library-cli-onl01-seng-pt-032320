3183f3442243e9e364b0e210b4f07b6b9aa5ede7
Dina Kendle <dina.kendle@gmail.com>
2020-05-02 17:26:42 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -35,6 +35,7 @@ class MusicLibraryController
   def list_artists
     Artist.all.sort {|a, b| a.name <=> b.name}.uniq.each.with_index(1) do |artist, i|
       puts " #{i}. #{artist.name}"
+    end
   end
 
 
