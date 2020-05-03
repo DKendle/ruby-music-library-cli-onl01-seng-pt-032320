3711ce1fb88a68ffef8157384ae4d24175729a00
Dina Kendle <dina.kendle@gmail.com>
2020-05-03 01:02:02 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -106,7 +106,7 @@ class MusicLibraryController
           #   song = list_of_songs[input + 2]
           #   puts "Playing #{song.name} by #{song.artist.name}"
           # else
-          end
+          # end
 binding.pry
         end
 
