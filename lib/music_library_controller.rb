70fb409f998ce8c90a72526ef75d207d8bba66c2
Dina Kendle <dina.kendle@gmail.com>
2020-05-03 00:37:12 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -105,6 +105,7 @@ class MusicLibraryController
           if (1..Song.all.length).include?(input)
             song = list_of_songs[input+2]
             puts "Playing #{song.name} by #{song.artist.name}"
+          else
           end
         end
 
