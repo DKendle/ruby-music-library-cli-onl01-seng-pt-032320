024391a1b7ff9e7eba645c662af15fed99860c34
Dina Kendle <dina.kendle@gmail.com>
2020-05-03 01:52:34 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -81,34 +81,33 @@ class MusicLibraryController
   end
 
 
-  # def play_song
-  #   #binding.pry
-  #     puts "Which song number would you like to play?"
-  #     #self.list_songs
-  #     input = gets.chomp.to_i
-  #     self.list_songs{|song_obj| song_obj[input-1]}
-#binding.pry
-      # if (input > 0) && (input <= Song.all.length)
-        # Song.all.sort{|indexed_song_obj| indexed_song_obj[[input-1]] }
-        # song = Song.all.sort{|a, b| a.name <=> b.name}[input-1]
-#binding.pry
-        #puts "Playing #{song.name} by #{song.artist.name}"
+  def play_song
+      puts "Which song number would you like to play?"
+        # self.list_songs
+      input = gets.chomp.to_i
+        if (input > 0) && (input <= Song.all.length)
+          # Song.all.sort{|indexed_song_obj| indexed_song_obj[[input-1]] }
+          song = Song.all.sort{|a, b| a.name <=> b.name}.uniq[input-1]
+          puts "Playing #{song.name} by #{song.artist.name}" if song
+            end
+          end
 
 
-        def play_song
-          puts "Which song number would you like to play?"
-          list_of_songs =  Song.all.sort{ |a, b| a.name <=> b.name }
-          #self.list_songs
-          input = gets.chomp.to_i
-
-          if (1..Song.all.length).include?(input)
-             song = list_of_songs[input+2]
-             puts "Playing #{song.name} by #{song.artist.name}"
-           else
-             #binding.pry
-           end
-        end
-
+        # def play_song
+        #   puts "Which song number would you like to play?"
+        #   list_of_songs =  Song.all.sort{ |a, b| a.name <=> b.name }
+        #   input = gets.chomp.to_i
+        # 
+        #   
+        #   # 
+        #   # if (1..Song.all.length).include?(input)
+        #   #    song = list_of_songs[input+2]
+        #   #    puts "Playing #{song.name} by #{song.artist.name}"
+        #   #  else
+        #   #    #binding.pry
+        #   #  end
+        # end
+        # 
 
 
 end
